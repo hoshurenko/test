a639eaec83bd13a02cd29763d39160d713717eae
hoshurenko <57762421+hoshurenko@users.noreply.github.com>
2020-02-10 12:45:57 +0200

    Update Hello.java

--- src/main/java/next/Hello.java
+++ src/main/java/next/Hello.java
@@ -3,5 +3,6 @@ package next;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello! Hi. Editions");
+        System.out.println("Second line");
     }
 }
