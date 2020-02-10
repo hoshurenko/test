179968c0230e3520c5835a266b9f59aba25a00ef
reone <goshurenko.aa@gmail.com>
2020-02-10 12:49:12 +0200

    some edits

--- src/main/java/next/Hello.java
+++ src/main/java/next/Hello.java
@@ -2,7 +2,13 @@ package next;
 
 public class Hello {
     public static void main(String[] args) {
+        int[] array = new int[]{2, 234, 43};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "; ");
+        }
+        System.out.println();
         System.out.println("Hello! Hi. Editions");
         System.out.println("Second line");
+        System.out.println("Third line");
     }
 }
