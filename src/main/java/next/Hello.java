44b196012b3b8a90c11f426445059c7801b130fd
reone <goshurenko.aa@gmail.com>
2020-02-10 12:44:20 +0200

    some edits

--- src/main/java/next/Hello.java
+++ src/main/java/next/Hello.java
@@ -2,6 +2,6 @@ package next;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello! Hi");
+        System.out.println("Hello! Hi. Editions");
     }
 }
